0fa71b3a5fbbb19c597b3179f89de0c8ddb88c36
bushraYounes <boushra.ali.younes@gmail.com>
2024-05-12 01:58:24 +0300

    sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/user/register', [UserController::class, 'createUser']);
+Route::post('/user/login', [UserController::class, 'loginUser']);
+
+
+Route::middleware("auth:sanctum")->group(function () {
+   
 });
 
 Route::get('authors', [AuthorController::class, 'index']);
