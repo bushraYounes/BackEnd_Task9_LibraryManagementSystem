6d71355e108340d65444fb8726cc9745dc79055a
bushraYounes <boushra.ali.younes@gmail.com>
2024-05-12 03:35:24 +0300

    reviews belong to user

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,11 +14,16 @@ class Review extends Model
     protected $fillable = [
         'review',
         'reviewable_type',
-        'reviewable_id'
+        'reviewable_id',
+        'user_id',
     ];
 
     public function reviewable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
--- database/migrations/2024_05_11_231055_create_reviews_table.php
+++ database/migrations/2024_05_11_231055_create_reviews_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('review');
             $table->morphs('reviewable');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
